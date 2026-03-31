b741e57cf2074d173c7eba201050fc4790fef161
Yicheng <yicheng-chen@outlook.com>
2026-03-31 19:31:30 +0900

    [multilink_copilot] Add bash script for recording rosbag

---
+++ aerial_motion/multilink_copilot/bin/rosbag.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ $# -gt 0 ]; then
+  robot_ns="$1"
+  shift
+else
+  robot_ns="dragon"
+fi
+
+topics=(
+  "$robot_ns/baro"
+  "$robot_ns/battery_voltage_status"
+  "$robot_ns/debug/four_axes/gain"
+  "$robot_ns/debug/pose/pid"
+  "$robot_ns/desire_coordinate"
+  "$robot_ns/flight_config_ack"
+  "$robot_ns/flight_config_cmd"
+  "$robot_ns/flight_state"
+  "$robot_ns/four_axes/command"
+  "$robot_ns/full_state_target"
+  "$robot_ns/ground_truth"
+  "$robot_ns/imu"
+  "$robot_ns/joint_states"
+  "$robot_ns/joints_ctrl"
+  "$robot_ns/joy"
+  "$robot_ns/mocap/ground_pose"
+  "$robot_ns/mocap/pose"
+  "$robot_ns/motor_info"
+  "$robot_ns/motor_pwms"
+  "$robot_ns/root/pose"
+  "$robot_ns/root/target_pose"
+  "$robot_ns/root/tail_pose"
+  "$robot_ns/rpy/gain"
+  "$robot_ns/rpy/pid"
+  "$robot_ns/servo/states"
+  "$robot_ns/servo/target_states"
+  "$robot_ns/servo/torque_enable"
+  "$robot_ns/target_pose"
+  "$robot_ns/target_rotation_motion"
+  "$robot_ns/trajectory_visualization"
+  "$robot_ns/uav/baselink/odom"
+  "$robot_ns/uav/cog/odom"
+  "$robot_ns/uav/full_state"
+  "$robot_ns/uav/nav"
+  "$robot_ns/uav_info"
+  "$robot_ns/uav_power"
+  "/tf"
+  "/tf_static"
+  "rosout"
+  "rosout_agg"
+)
+
+exec rosbag record "${topics[@]}" "$@"
